a327c255f9707665e52dd6d040b53ba087c66884
eranharel <harel.eran@gmail.com>
2019-05-21 15:39:54 +0300

    removed UDP support

--- src/main/java/com/outbrain/gruffalo/netty/GruffaloProxy.java
+++ src/main/java/com/outbrain/gruffalo/netty/GruffaloProxy.java
@@ -1,34 +1,31 @@
 package com.outbrain.gruffalo.netty;
 
 import com.google.common.base.Preconditions;
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.socket.nio.NioDatagramChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 class GruffaloProxy {
 
   private static final Logger log = LoggerFactory.getLogger(GruffaloProxy.class);
   private final ChannelFuture tcpChannelFuture;
-  private final ChannelFuture udpChannelFuture;
   private final EventLoopGroup eventLoopGroup;
   private final Throttler throttler;
 
-  public GruffaloProxy(final EventLoopGroup eventLoopGroup, final TcpServerPipelineFactory tcpServerPipelineFactory,
-                       final UdpServerPipelineFactory udpServerPipelineFactory, final int tcpPort, final int udpPort, final Throttler throttler) throws InterruptedException {
+  public GruffaloProxy(final EventLoopGroup eventLoopGroup,
+                       final TcpServerPipelineFactory tcpServerPipelineFactory,
+                       final int tcpPort,
+                       final Throttler throttler) {
     this.throttler = Preconditions.checkNotNull(throttler, "throttler must not be null");
     this.eventLoopGroup = Preconditions.checkNotNull(eventLoopGroup, "eventLoopGroup must not be null");
     tcpChannelFuture = createTcpBootstrap(tcpServerPipelineFactory, tcpPort);
-    udpChannelFuture = createUdpBootstrap(udpServerPipelineFactory, udpPort);
     log.info("Initialization completed");
   }
 
@@ -36,17 +33,7 @@ class GruffaloProxy {
     new ClassPathXmlApplicationContext("classpath:applicationContext-GruffaloLib-all.xml");
   }
 
-  private ChannelFuture createUdpBootstrap(final UdpServerPipelineFactory udpServerPipelineFactory, final int udpPort) throws InterruptedException {
-    log.info("Initializing UDP...");
-    Bootstrap udpBootstrap = new Bootstrap();
-    udpBootstrap.group(eventLoopGroup).channel(NioDatagramChannel.class).handler(udpServerPipelineFactory);
-
-    final ChannelFuture channelFuture = udpBootstrap.bind(udpPort);
-    log.info("Binding to UDP port {}", udpPort);
-    return channelFuture;
-  }
-
-  private ChannelFuture createTcpBootstrap(final TcpServerPipelineFactory tcpServerPipelineFactory, final int tcpPort) throws InterruptedException {
+  private ChannelFuture createTcpBootstrap(final TcpServerPipelineFactory tcpServerPipelineFactory, final int tcpPort) {
     log.info("Initializing TCP...");
     ServerBootstrap tcpBootstrap = new ServerBootstrap();
     tcpBootstrap.group(eventLoopGroup);
@@ -54,12 +41,7 @@ class GruffaloProxy {
     tcpBootstrap.childHandler(tcpServerPipelineFactory);
     tcpBootstrap.option(ChannelOption.ALLOCATOR, UnpooledByteBufAllocator.DEFAULT);
 
-    final ChannelFuture channelFuture = tcpBootstrap.bind(tcpPort).addListener(new ChannelFutureListener() {
-      @Override
-      public void operationComplete(final ChannelFuture future) throws Exception {
-        throttler.setServerChannel(future.channel());
-      }
-    });
+    final ChannelFuture channelFuture = tcpBootstrap.bind(tcpPort).addListener((ChannelFutureListener) future -> throttler.setServerChannel(future.channel()));
     log.info("Binding to TCP port {}", tcpPort);
     return channelFuture;
   }
@@ -67,7 +49,6 @@ class GruffaloProxy {
   public void shutdown() throws InterruptedException {
     log.info("Shutting down inbound channels...");
     tcpChannelFuture.sync().channel().closeFuture().await(200);
-    udpChannelFuture.sync().channel().closeFuture().await(200);
 
     log.info("Shutting down server...");
     eventLoopGroup.shutdownGracefully();
