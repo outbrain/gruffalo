8760028649db8d9f6c94ce5faae22390ff05efa5
eranharel <harel.eran@gmail.com>
2019-05-25 14:52:35 +0300

    logback migration

--- gruffalo-standalone.sh
+++ gruffalo-standalone.sh
@@ -2,5 +2,4 @@
 
 mvn dependency:build-classpath -Dmdep.outputFile=classpath
 CLASSPATH=target/classes:`cat classpath`
-log4jfile=$(pwd)/src/main/resources/stand-alone-log4j.xml
-java -cp $CLASSPATH -Dlog4j.configuration=file://$log4jfile -Xmx2024m -Xms2024m com.outbrain.gruffalo.StandaloneGruffaloServer "$@"
+java -cp $CLASSPATH -Xmx2024m -Xms2024m com.outbrain.gruffalo.StandaloneGruffaloServer "$@"
